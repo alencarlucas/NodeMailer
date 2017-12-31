8c5bdb099f0d13b16a4042a19ef5b4b056750d3b
alencarlucas <alencarlucaas@gmail.com>
2017-12-31 13:28:53 -0200

    mongoDB login flow added

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const express = require("express");
-const passportConfig = require("./services/passport");
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-require("./routes/authRoutes")(app);
+app.use(
+	cookieSession({
+		//set expire time to 30 days (in milisec)
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		//used to encripty cookie
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
+/*- 2 vagas para Desenvolvedor React Native
+
+Oportunidades CLT e PJ
+
+Interessados enviar o CV com o título da vaga para TALENTOS@CROSOFTEN.COM
+*/
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,23 @@
-const passport = require("passport");
+const passport = require('passport');
 
 module.exports = app => {
 	app.get(
 		// route path
-		"/auth/google",
-		passport.authenticate("google", {
+		'/auth/google',
+		passport.authenticate('google', {
 			// define the information from user that we need
-			scope: ["profile", "email"]
+			scope: ['profile', 'email']
 		})
 	);
 
-	app.get("/auth/google/callback", passport.authenticate("google"));
+	app.get('/auth/google/callback', passport.authenticate('google'));
+
+	app.get('/api/logout', (req, res) => {
+		req.logout();
+		res.send(req.user);
+	});
+
+	app.get('/api/current_user', (req, res) => {
+		res.send(req.user);
+	});
 };
